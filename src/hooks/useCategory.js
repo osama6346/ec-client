b0b9a60d3ff4fd4c246faabe9e20ab726d3d1e25
Osama Iqbal <96139862+osama6346@users.noreply.github.com>
2023-06-04 14:37:39 +0500

    First commit

---
+++ src/hooks/useCategory.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {Url} from "../components/Url";
+
+export default function useCategory() {
+  const [categories, setCategories] = useState([]);
+
+  //get cat
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get(`${Url}/api/v1/category/get-category`);
+      setCategories(data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return categories;
+}
